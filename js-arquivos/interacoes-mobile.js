af2fb0e1f583fe3d73bf0dab3aa234a88df1fcaf
LeonardoHenrique <154704500+LeonardoHenriqueDC@users.noreply.github.com>
2024-03-01 13:48:00 -0400

    JS 1.0

--- js-arquivos/interacoes-mobile.js
+++ js-arquivos/interacoes-mobile.js
@@ -1,1 +1,48 @@
+// Criando a função para menu Hamburguer 
 
+let burguer = document.querySelector("#menu-mobile")
+let list = document.querySelector("#list")
+
+function menu(){
+    if(list.style.display === 'none'){
+        list.style.display = 'flex'
+    }
+    else{
+        list.style.display = 'none'
+    }
+}
+
+
+burguer.addEventListener('click', menu)
+
+//-----------------------------------------------
+
+// Criando interação com menu 
+
+let btHome = document.querySelector("#bt-home")
+let btPortifolio = document.querySelector("#bt-portifolio")
+
+let header = document.querySelector("#header")
+let main = document.querySelector("#main")
+
+
+// Função home 
+function botaoHome(){
+    if(header.style.display === 'none' && main.style.display === 'flex'){
+        header.style.display = 'grid'
+        main.style.display = 'none'
+    }
+}
+
+btHome.addEventListener('click', botaoHome)
+
+// Função Portifolio 
+
+function botaoPortifolio(){
+    if(main.style.display === 'none' && header.style.display === 'grid'){
+        main.style.display = 'flex'
+        header.style.display = 'none'
+    } 
+}
+
+btPortifolio.addEventListener('click', botaoPortifolio)
